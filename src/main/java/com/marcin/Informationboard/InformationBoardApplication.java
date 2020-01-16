6dc75c3d07a8950cc6b47a19462cac0e6345e222
Marcin Stanisławek <mstanislawek@gmail.com>
2020-01-16 18:03:09 +0100

    Init repo

---
+++ src/main/java/com/marcin/Informationboard/InformationBoardApplication.java
@@ -0,0 +1,13 @@
+package com.marcin.Informationboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcin/Informationboard/InformationBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcin.Informationboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
